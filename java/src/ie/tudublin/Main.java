b5aa901f00ae504afcf5d8f028a1f4e390f9eb2e
Daniel Carson <danielcarson@Daniels-Laptop.station>
2024-03-27 20:42:20 +0000

    Edited the main function to work with my new project file.

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,14 +3,15 @@ package ie.tudublin;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
+import example.CubeVisual1;
 
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new MyVisual());
+        processing.core.PApplet.runSketch(a, new DanielsVisual());
     }
-
+    
     public static void main(String[] args) {
         Main main = new Main();
         main.startUI();
